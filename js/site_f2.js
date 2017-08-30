6c775136ffa66a6a7bd2385d27df7ea154f543e1
Camargo Gomes <camargo.gomes@ifnet-for.ifactory.com.br>
2017-08-30 15:07:09 -0300

    novo

--- js/site_f2.js
+++ js/site_f2.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){
-    function createNode(element) {
+ 
+
+});
+
+
+function tst(){
+  
+      function createNode(element) {
         return document.createElement(element);
     }
   
-    function append(parent, el) {
-      return parent.appendChild(el);
+      const tr1 = document.getElementById('resultados');
+      const url = 'http://ergast.com/api/f1/drivers.json';
+      fetch(url)
+      .then((resp) => resp.json())
+      .then(function(data) {
+        let drivers = data.MRData.DriverTable.Drivers;
+        return drivers.map(function(driver) {
+          tr2 = $('<tr/>');
+          url1 = driver.url;
+          tr2.append("<td>" + driver.driverId + "</td>");
+          tr2.append("<td id='url1'><a href=" +   driver.url + "</a>Link</td>");
+          $('table').append(tr2);
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });  
     }
   
-    const tr1 = document.getElementById('resultados');
-    const url = 'http://ergast.com/api/f1/drivers.json';
-    fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-      let drivers = data.MRData.DriverTable.Drivers;
-      return drivers.map(function(driver) {
-        let td = createNode('td');
-        tr = createNode('tr');
-        tr.innerHTML = `${driver.driverId} ${driver.url}`;
-        append(td, tr)
-        append(tr1,td);
-      });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });   
-});
+
